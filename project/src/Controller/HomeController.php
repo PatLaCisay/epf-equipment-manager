622eaac41282ffbe893bafe1761652470d396b17
PatLaCisay <lucasbarreaupub@gmail.com>
2023-04-01 15:24:35 +0200

    Feat #8 : add space in Hcontroller

--- project/src/Controller/HomeController.php
+++ project/src/Controller/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(): Response
-    {
+    { 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
