1bb2abf1569e186c6ece238b2fe2831ebffa17bf
Émile <emile.royer@epfedu.fr>
2023-04-03 20:55:02 +0200

    feat: Add a form to create categories
    Once created, they are viewable directly in the database.

--- project/src/Controller/CategoryController.php
+++ project/src/Controller/CategoryController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,10 +22,24 @@ class CategoryController extends AbstractController
     }
 
     #[Route('/category/add', name: 'app_add_category')]
-    public function add(): Response
+    public function add(Request $request, ManagerRegistry $doctrine): Response
     {
-        return $this->render('category/add.html.twig', [
+        $repo = new CategoryRepository($doctrine);
+
+        $category = new Category();
+
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $repo->add($form->getData(), true);
+
+            return $this->redirectToRoute('app_category');
+        }
+
+        return $this->renderForm('category/add.html.twig', [
             'controller_name' => 'CategoryController',
+            'form' => $form,
         ]);
     }
 }
