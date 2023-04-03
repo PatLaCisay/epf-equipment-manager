f20b225152a86a8ea74d6680352180131dab3113
Émile <emile.royer@epfedu.fr>
2023-04-03 23:58:46 +0200

    Fix: Add a name field to categories
    Enables a better identification of them.

--- project/src/Entity/Category.php
+++ project/src/Entity/Category.php
@@ -15,6 +15,9 @@ class Category
     #[ORM\Column(type: 'integer')]
     private $id;
 
+    #[ORM\Column(type: 'string', length: 255, unique: true)]
+    private $name;
+
     #[ORM\Column(type: 'blob', nullable: true)]
     private $image;
 
@@ -38,6 +41,18 @@ class Category
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
     }
 
     public function getImage()
