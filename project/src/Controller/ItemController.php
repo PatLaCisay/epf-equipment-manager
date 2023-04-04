b28f1f7d55fd27e4cd05c5eca928a6bfa954b59b
Émile <emile.royer@epfedu.fr>
2023-04-04 02:04:54 +0200

    feat: Implement a form to create new items

--- project/src/Controller/ItemController.php
+++ project/src/Controller/ItemController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Item;
+use App\Form\ItemType;
+use App\Repository\ItemRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,10 +22,23 @@ class ItemController extends AbstractController
     }
 
     #[Route('/item/add', name: 'app_add_item')]
-    public function add(): Response
+    public function add(Request $request, ManagerRegistry $doctrine): Response
     {
-        return $this->render('item/add.html.twig', [
+        $repo = new ItemRepository($doctrine);
+
+        $item = new Item();
+        $form = $this->createForm(ItemType::class, $item);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $repo->add($form->getData(), true);
+
+            return $this->redirectToRoute('app_item');
+        }
+
+        return $this->renderForm('item/add.html.twig', [
             'controller_name' => 'ItemController',
+            "form" => $form,
         ]);
     }
 }
