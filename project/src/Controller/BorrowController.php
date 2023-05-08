b3af6ef12ee061748177da0c48b703cbde4c8f12
Minigandii <clement.gandillon@gmail.com>
2023-05-08 17:41:43 +0200

    feat/10 creat a Borrow done

---
+++ project/src/Controller/BorrowController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Borrow;
+use App\Form\BorrowType;
+use App\Repository\BorrowRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BorrowController extends AbstractController
+{
+    #[Route('/borrow', name: 'app_borrow')]
+    public function index(): Response
+    {
+        return $this->render('borrow/index.html.twig', [
+            'controller_name' => 'BorrowController',
+        ]);
+    }
+
+    #[Route('/borrow/add', name: 'app_add_borrow')]
+    public function add(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $repo = new BorrowRepository($doctrine);
+
+        $borrow = new Borrow();
+        $form = $this->createForm(BorrowType::class, $borrow);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $repo->add($form->getData(), true);
+            
+            return $this->redirectToRoute('app_borrow');
+        }
+
+        return $this->render('borrow/add.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
+}
---
+++ project/src/Form/BorrowType.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Item;
+use App\Entity\Room;
+use App\Entity\User;
+use App\Entity\Group;
+use App\Entity\Borrow;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BorrowType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('startDate', null, ['data' => new \DateTime(),])
+            ->add('endDate')
+            ->add('description')
+            ->add('quantity')
+            //->add('stakeholder')
+            ->add('stakeholder', EntityType::class, [
+                'class' => User::class,
+                // sort stakeholder by alphabetical order
+                'query_builder' => function (EntityRepository $repository) {
+                    return $repository->createQueryBuilder('user')
+                        ->select('user')
+                        ->orderBy('user.email', 'ASC');
+                },
+                'choice_label' => function (User $user) {
+                    return $user->getEmail();
+                }
+            ])
+            ->add('room', EntityType::class, [
+                'class' => Room::class,
+                'query_builder' => function (EntityRepository $repository) {
+                    return $repository->createQueryBuilder('room')
+                        ->select('room')
+                        ->orderBy('room.name', 'ASC');
+                },
+                'choice_label' => function (Room $room) {
+                    return $room->getName();
+                }
+            ])
+            //->add('items')
+            ->add('items', EntityType::class, [
+                'class' => Item::class,
+                'query_builder' => function (EntityRepository $repository) {
+                    return $repository->createQueryBuilder('items')
+                        ->select('items')
+                        ->orderBy('items.name', 'ASC');
+                },
+                'choice_label' => function (Item $items) {
+                    return $items->getName();
+                },
+                'mapped' => false
+            ])
+            //->add('team')
+            ->add('team', EntityType::class, [
+                'class' => Group::class,
+                'query_builder' => function (EntityRepository $repository) {
+                    return $repository->createQueryBuilder('g')
+                        ->select('g')
+                        ->orderBy('g.name', 'ASC');
+                },
+                'choice_label' => function (Group $group) {
+                    return $group->getName();
+                }
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Borrow::class,
+        ]);
+    }
+}
