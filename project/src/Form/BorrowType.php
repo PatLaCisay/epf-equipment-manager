aa572c7132e833c84b485420f0fed19a690ccefa
Minigandii <clement.gandillon@gmail.com>
2023-05-10 17:04:01 +0200

    class imports were missing

--- project/src/Form/BorrowType.php
+++ project/src/Form/BorrowType.php
@@ -12,6 +12,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 class BorrowType extends AbstractType
 {
