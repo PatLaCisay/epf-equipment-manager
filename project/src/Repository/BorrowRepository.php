3c588fd5604bc4f712ad7718825ec90c1dbc3e96
Lucas BARREAU <65079955+PatLaCisay@users.noreply.github.com>
2023-06-02 16:13:40 +0200

    Feat #59 : user deletion with control (#72)
    * fixing research of groups with opened borrows
    
    * Harmonizing code
    
    * implementing requested changes

--- project/src/Repository/BorrowRepository.php
+++ project/src/Repository/BorrowRepository.php
@@ -2,9 +2,10 @@
 
 namespace App\Repository;
 
+use App\Entity\Group;
 use App\Entity\Borrow;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<Borrow>
