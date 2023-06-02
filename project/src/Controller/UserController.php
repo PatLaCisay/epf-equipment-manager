40e2c86fad37815738743d3a1d2dffb54b99a53f
Lucas BARREAU <65079955+PatLaCisay@users.noreply.github.com>
2023-06-02 16:13:40 +0200

    Feat #59 : user deletion with control (#72)
    * fixing research of groups with opened borrows
    
    * Harmonizing code
    
    * implementing requested changes

--- project/src/Controller/UserController.php
+++ project/src/Controller/UserController.php
@@ -7,6 +7,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -20,47 +21,52 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/user/delete/{id}', name: 'app_user')]
-    public function delete(Request $request, $id,  ManagerRegistry $doctrine): Response
+    #[Route('/user/delete/{id}', name: 'app_delete_user')]
+    public function delete(Request $request, User $user,  ManagerRegistry $doctrine): Response
     {
         $entityManager= $doctrine->getManager();
-        if($id==null){
-            return $this->render('user/index.html.twig');
-        }
-        
-        $user = $entityManager
-            ->getRepository(User::class)
-            ->find($id);
+        $userRepo = $entityManager->getRepository(User::class);
 
-        if (!$user) {
-            throw $this->createNotFoundException(
-                "Aucun utilisateur trouvé"
-            );    
-        }
+        $message = 'Vous allez supprimer '.$user->getFirstName().' '.$user->getLastName().' de la BDD. Cette action est irréversible.';
 
-        $form = $this->createFormBuilder($user)
-            ->add('proceed', SubmitType::class, ['label' => 'Proceed'])
-            ->add('abort', SubmitType::class, ['label' => 'Abort'])
-            ->getForm();
-        
-        $form->handleRequest($request);
+        if($userRepo->isDeletable($user)){
+            $form = $this->createFormBuilder()
+                ->add('proceed', SubmitType::class, ['label' => 'Proceed'])
+                ->add('abort', SubmitType::class, ['label' => 'Abort'])
+                ->getForm();
+            
+            $form->handleRequest($request);
 
-        if($form->isSubmitted())
-        {
-            if($form->get('abort')->isClicked()) //this method works and is imported from 'Symfony\Component\Form\Extension\Core\Type\SubmitType' but isn't recognized
+            if($form->isSubmitted())
             {
-                return $this->redirectToRoute("app_home");
+                if($form->get('abort')->isClicked()) //this method works and is imported from 'Symfony\Component\Form\Extension\Core\Type\SubmitType' but isn't recognized
+                {
+                    return $this->redirectToRoute("app_home");
+                }
+                if ($form->get('proceed')->isClicked())
+                {            
+                    $entityManager->remove($user);
+                    $entityManager->flush();
+                    return $this->redirectToRoute("app_home");
+                }
             }
-            if ($form->get('proceed')->isClicked())
-            {            
-                $entityManager->remove($user);
-                $entityManager->flush();
+        }else{
+            $form = $this->createFormBuilder()
+                ->add('abort', SubmitType::class, ['label' => 'Abort'])
+                ->getForm();
+            
+            $form->handleRequest($request);
+
+            $message="Impossible de supprimer ".$user->getFirstName()." ".$user->getLastName()." de la BDD. Cet utilisateur a des emprunts en cours ou est administrateur.";
+            
+            if($form->isSubmitted())
+            {
                 return $this->redirectToRoute("app_home");
             }
         }
         return $this->render('user/delete.html.twig', [
-            'form'=>$form->createView(),
-            'user'=>$user
+            'message'=>$message,
+            'form'=>$form->createView()
         ]);
     }
 }
