935cb269c760727f4eb425e230f48f501b63be74
suprkco <kilian.codaccioni@gmail.com>
2023-06-07 10:03:56 +0200

    search fonctionality connected to front end

--- project/src/Controller/UnifiedSearchController.php
+++ project/src/Controller/UnifiedSearchController.php
@@ -15,43 +15,73 @@ class UnifiedSearchController extends AbstractController
     #[Route('/search', name: 'app_unified_search')]
     public function index(Request $request, ManagerRegistry $doctrine): Response
     {
-        $form = $this->createForm(UnifiedSearchType::class);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $searched_name = $form->get('query')->getData();
-            $search_results = [];
+        // <form class="searchbar" action="/search/" method="get">
+        //         <input type="text" name="search" placeholder="Rechercher...">
+        //         <button class="search-button" type="submit">
+        //         <img class="icon" src="{{ asset('img/search.svg') }}" alt="Rechercher">
+        //     </button>
+        // </form>
 
-            switch ($form->get("criterion")->getData()) {
-                case "auto":
-                    $search_results = $doctrine->getRepository(Item::class)
-                        ->findByDefaultMatch($searched_name);
-                    break;
+        $form = $request->query->get('search');
 
-                case "item":
-                    $search_results = $doctrine->getRepository(Item::class)
-                        ->findByNameMatch($searched_name);
-                    break;
-
-                case "cat":
-                    $search_results = $doctrine->getRepository(Item::class)
-                        ->findByCategoryMatch($searched_name);
-                    break;
-
-                default:
-                    throw new \Exception("Unknow criterion selected.", 1);
-            }
+        if ($form) {
+            $searched_name = $form;
+            $search_results = $doctrine->getRepository(Item::class)
+                        ->findByDefaultMatch($form);
 
             return $this->renderForm('unified_search/index.html.twig', [
                 'controller_name' => 'UnifiedSearchController',
-                'form' => $form,
-                "results" => $search_results,
+                'keyword' => $searched_name,
+                'results' => $search_results,
             ]);
         }
 
         return $this->renderForm('unified_search/index.html.twig', [
             'controller_name' => 'UnifiedSearchController',
-            'form' => $form,
         ]);
     }
+
+    // #[Route('/search', name: 'app_unified_search')]
+    // public function index(Request $request, ManagerRegistry $doctrine): Response
+    // {
+    //     $form = $this->createForm(UnifiedSearchType::class);
+    //     $form->handleRequest($request);
+
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $searched_name = $form->get('query')->getData();
+    //         $search_results = [];
+
+    //         switch ($form->get("criterion")->getData()) {
+    //             case "auto":
+    //                 $search_results = $doctrine->getRepository(Item::class)
+    //                     ->findByDefaultMatch($searched_name);
+    //                 break;
+
+    //             case "item":
+    //                 $search_results = $doctrine->getRepository(Item::class)
+    //                     ->findByNameMatch($searched_name);
+    //                 break;
+
+    //             case "cat":
+    //                 $search_results = $doctrine->getRepository(Item::class)
+    //                     ->findByCategoryMatch($searched_name);
+    //                 break;
+
+    //             default:
+    //                 throw new \Exception("Unknow criterion selected.", 1);
+    //         }
+
+    //         return $this->renderForm('unified_search/index.html.twig', [
+    //             'controller_name' => 'UnifiedSearchController',
+    //             'form' => $form,
+    //             "results" => $search_results,
+    //         ]);
+    //     }
+
+    //     return $this->renderForm('unified_search/index.html.twig', [
+    //         'controller_name' => 'UnifiedSearchController',
+    //         'form' => $form,
+    //     ]);
+    // }
 }
